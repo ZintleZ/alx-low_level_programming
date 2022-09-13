06c55d05405ee50e4ea6c51b8cc66eef1809e9d9
ZintleZide <sesethuz@gmail.com>
2022-09-13 11:00:14 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char h[] = "_putchar";
+	int m;
+
+	m = 0;
+
+	while (m <= 7)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
+	return (0);
+}
